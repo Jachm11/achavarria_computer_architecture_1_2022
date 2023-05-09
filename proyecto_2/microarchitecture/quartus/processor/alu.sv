cdd2b6be41f185025963bd2f54d82fba5e6b4e00
Jose Alejandro Chavarria <joalchama@gmail.com>
2023-05-08 22:50:32 -0600

    Add control unit

--- proyecto_2/microarchitecture/quartus/processor/alu.sv
+++ proyecto_2/microarchitecture/quartus/processor/alu.sv
@@ -1,31 +1,27 @@
 module alu #(parameter N=32) (
-	input  logic[3:0] operation,
+	input  logic[2:0] operation,
 	input  logic[N-1:0] a,
 	input  logic[N-1:0] b,
 	output logic[N-1:0] out
 );
 
-	localparam OP0 = 4'b0000; //SUM
-	localparam OP1 = 4'b0001; //SUB
-	localparam OP2 = 4'b0010; //XOR
-	localparam OP3 = 4'b0011; //OR
-	localparam OP4 = 4'b0100; //AND
-	localparam OP5 = 4'b0101; //SLL
-	localparam OP6 = 4'b0110; //SRL
-	localparam OP7 = 4'b0111; //SRA
-	localparam OP8 = 4'b1000; //SLT
+	localparam OP0 = 3'b000; //SUM
+	localparam OP1 = 3'b001; //SUB
+	localparam OP2 = 3'b010; //MUL
+	localparam OP3 = 3'b011; //DIV
+	localparam OP4 = 3'b100; //MOD
+	localparam OP5 = 3'b101; //SLL
+	localparam OP6 = 3'b110; //SRL
 
 	always_comb begin
 		case(operation)
 			OP0: out = a + b;
 			OP1: out = a - b;
-			OP2: out = a ^ b;
-			OP3: out = a | b;
-			OP4: out = a & b;
+			OP2: out = a * b;
+			OP3: out = a / b;
+			OP4: out = a % b;
 			OP5: out = a << b;
 			OP6: out = a >> b;
-			OP7: out = a >>> b;
-			OP8: out = a < b ? 1 : 0;
 			default: out = 0;
 		endcase
 	end
