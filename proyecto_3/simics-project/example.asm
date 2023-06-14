3eba457f35b6563a57b38b34d54ba879c02d2006
sebas-mora28 <semasmora@gmail.com>
2023-06-13 23:25:33 -0600

    Proyecto 3

---
+++ proyecto_3/simics-project/example.asm
@@ -0,0 +1,44 @@
+_start: 
+
+mov edi, 0
+mov esi, 0
+
+
+xor edx, edx
+mov eax, 10000
+mov ecx, 10000
+div ecx
+add esi, eax 
+add edi, 1
+xor edx, edx 
+mov eax, 10000 
+mov ecx, 5000
+div ecx
+add esi, eax
+add edi, 1 
+
+xor edx, edx
+mov eax, 10000 
+mov ecx, 7000 
+div ecx 
+add esi, eax
+add edi, 1 
+
+
+
+xor edx, edx 
+mov eax, 10000
+div esi
+
+xor eax, eax 
+xor eax, eax 
+xor eax, eax
+
+
+
+
+
+
+
+
+
---
+++ proyecto_3/simics-project/linux64/obj/modules/dma-device/dma-device-dml.d
@@ -0,0 +1,378 @@
+dma-device-dml.d dma-device-dml.o: dma-device-dml-cdep.c \
+ /usr/include/stdc-predef.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/devs/io-memory.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/types.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base-types.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/module-host-config.h \
+ /usr/include/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/libio.h \
+ /usr/include/x86_64-linux-gnu/bits/_G_config.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/alloca.h /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/string.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/strings.h /usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h \
+ /usr/include/stdint.h /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/host-info.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdbool.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/memory-transaction.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/attr-value.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/version.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/help-macros.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/strbuf.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/alloc.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/memory.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/pywrap.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/instrumentation-provider.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/bank-instrumentation.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/conf-object.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/sobject.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/vect.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/int-register.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/register-view.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/register-view-read-only.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/hashtab.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/simulator/python.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/simulator/sim-get-class.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/linux64/bin/dml/include/simics/dmllib.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/log.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/swabber.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/simulator/conf-object.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/build-id-6.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/build-id.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/dbuffer.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/frags.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/callbacks.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/arith.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/int128.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/duration.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/bigtime.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/local-time.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/cycle.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/event.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/time.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/clock.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/configuration.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/global-notifier.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/hap-producer.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/notifier.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/object-locks.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/sim-exception.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/obsolete/4_8.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/simulator/embed.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/obsolete/5.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/device-api.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/transaction.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/breakpoints.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/processor/types.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/map-target.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/direct-memory.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/devs/signal.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/devs/ram.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/cbdata.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/devs/translator.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/transaction.h \
+ /home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/devs/memory-space.h
+
+/usr/include/stdc-predef.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/devs/io-memory.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/types.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base-types.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/module-host-config.h:
+
+/usr/include/stdio.h:
+
+/usr/include/x86_64-linux-gnu/bits/libc-header-start.h:
+
+/usr/include/features.h:
+
+/usr/include/x86_64-linux-gnu/sys/cdefs.h:
+
+/usr/include/x86_64-linux-gnu/bits/wordsize.h:
+
+/usr/include/x86_64-linux-gnu/bits/long-double.h:
+
+/usr/include/x86_64-linux-gnu/gnu/stubs.h:
+
+/usr/include/x86_64-linux-gnu/gnu/stubs-64.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h:
+
+/usr/include/x86_64-linux-gnu/bits/types.h:
+
+/usr/include/x86_64-linux-gnu/bits/typesizes.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/__FILE.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/FILE.h:
+
+/usr/include/x86_64-linux-gnu/bits/libio.h:
+
+/usr/include/x86_64-linux-gnu/bits/_G_config.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/7/include/stdarg.h:
+
+/usr/include/x86_64-linux-gnu/bits/stdio_lim.h:
+
+/usr/include/x86_64-linux-gnu/bits/sys_errlist.h:
+
+/usr/include/stdlib.h:
+
+/usr/include/x86_64-linux-gnu/bits/waitflags.h:
+
+/usr/include/x86_64-linux-gnu/bits/waitstatus.h:
+
+/usr/include/x86_64-linux-gnu/bits/floatn.h:
+
+/usr/include/x86_64-linux-gnu/bits/floatn-common.h:
+
+/usr/include/x86_64-linux-gnu/sys/types.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/clock_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/clockid_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/time_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/timer_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/stdint-intn.h:
+
+/usr/include/endian.h:
+
+/usr/include/x86_64-linux-gnu/bits/endian.h:
+
+/usr/include/x86_64-linux-gnu/bits/byteswap.h:
+
+/usr/include/x86_64-linux-gnu/bits/byteswap-16.h:
+
+/usr/include/x86_64-linux-gnu/bits/uintn-identity.h:
+
+/usr/include/x86_64-linux-gnu/sys/select.h:
+
+/usr/include/x86_64-linux-gnu/bits/select.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/sigset_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h:
+
+/usr/include/x86_64-linux-gnu/sys/sysmacros.h:
+
+/usr/include/x86_64-linux-gnu/bits/sysmacros.h:
+
+/usr/include/x86_64-linux-gnu/bits/pthreadtypes.h:
+
+/usr/include/x86_64-linux-gnu/bits/thread-shared-types.h:
+
+/usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h:
+
+/usr/include/alloca.h:
+
+/usr/include/x86_64-linux-gnu/bits/stdlib-float.h:
+
+/usr/include/string.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/locale_t.h:
+
+/usr/include/x86_64-linux-gnu/bits/types/__locale_t.h:
+
+/usr/include/strings.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h:
+
+/usr/include/stdint.h:
+
+/usr/include/x86_64-linux-gnu/bits/wchar.h:
+
+/usr/include/x86_64-linux-gnu/bits/stdint-uintn.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/7/include-fixed/limits.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/7/include-fixed/syslimits.h:
+
+/usr/include/limits.h:
+
+/usr/include/x86_64-linux-gnu/bits/posix1_lim.h:
+
+/usr/include/x86_64-linux-gnu/bits/local_lim.h:
+
+/usr/include/linux/limits.h:
+
+/usr/include/x86_64-linux-gnu/bits/posix2_lim.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/host-info.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/7/include/stdbool.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/memory-transaction.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/attr-value.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/version.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/help-macros.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/strbuf.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/alloc.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/memory.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/pywrap.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/instrumentation-provider.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/bank-instrumentation.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/conf-object.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/sobject.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/vect.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/int-register.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/register-view.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/register-view-read-only.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/hashtab.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/simulator/python.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/simulator/sim-get-class.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/linux64/bin/dml/include/simics/dmllib.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/log.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/swabber.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/simulator/conf-object.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/build-id-6.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/build-id.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/dbuffer.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/frags.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/callbacks.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/arith.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/util/int128.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/duration.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/bigtime.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/local-time.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/cycle.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/event.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/time.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/clock.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/configuration.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/global-notifier.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/hap-producer.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/notifier.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/object-locks.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/sim-exception.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/obsolete/4_8.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/simulator/embed.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/obsolete/5.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/device-api.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/transaction.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/breakpoints.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/processor/types.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/map-target.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/direct-memory.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/devs/signal.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/devs/ram.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/base/cbdata.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/devs/translator.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/model-iface/transaction.h:
+
+/home/sebastian/Documents/arqui-proyecto-III/simics-6.0.166/src/include/simics/devs/memory-space.h:
